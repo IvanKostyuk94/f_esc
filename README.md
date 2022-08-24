d2e70dc6321d7ecb50931c925bf7c2aed19afa9a
Ivan Kostyuk <kostyuk@hotmail.de>
2022-08-24 17:01:01 +0200

    Cleaned notebooks and expanded README

--- README.md
+++ README.md
@@ -13,6 +13,8 @@ To use the notebooks the dataframes containing the halo information are needed. 
     <li><b>build_df.py:</b> Goes through the folder of simulation runs and creates a <a href="https://pandas.pydata.org/">pandas</a> dataframe conataining the properties of the examined halos</li>
     <li><b>update_df.py:</b></li> Adds several additional columns to the halo dataframe namely the ages of stellar particles in the halo as well as the gas clumping, surface stellar density and surface gas density of the central galaxy, defined to be enclosed by 0.2Rvir.
     <li><b>add_metallicities.py:</b></li> Adds an additional column to the dataframe containing stellar metallicities. Should be moved into <b>update_df.py</b> in the future
+    <li><b>prepare_new_runs.py:</b> Tools for preparing a new CRASH post-processing run.
+    <li><b>synchronize_folders.py:</b> Tools for moving halos from the RT simulations performed with a reduced number of stellar particles to the escape fraction calculation performed with the full set of stellar particles.
     <li><b>build_radius_df.py:</b></li> <b>Deprecated</b> code originally used to analyze the escape fraction at different distances from the halo center. Should this become necessary in the future I would recommend to change <b>build_df.py</b> with elements from this file.
     <li><b>fix_stellar_ages.py:</b></li> <b>Deprecated</b> code which was only used when <b>update_df.py</b> used the wrong stellar particles in a data frame update.
 </ul>  
@@ -28,6 +30,16 @@ To use the notebooks the dataframes containing the halo information are needed. 
     <li><b>halo_image.ipynb:</b> Used to create projected images of halos as seen in fig. 4. This notebook needs the full halo maps and therefore also the full simulation results.
     <li><b>large_radii.ipynb:</b> Used to analyze the escape fraction as a function of distance from the halo center. Not used in the final project.
     <li><b>loc_esc.ipynb:</b> Used to analyze the dependence the effect of using different local escape fractions as seen in fig. 7 of the paper.
+    <li><b>median_fesc.ipynb:</b> Used to analyze the dependence of the escape fraction as a function of stellar and halo, compare the escape fraction with and without dust and compare our results to literature. Figs. 2, 3 and 13 were created using this notebook.
+    <li><b>merge_sources.ipynb:</b> Used in the preprocessing of the halo runs in particular to reduce the number of sources. Also contains a number of tests run on the results. Requires the full simulation data to work with.
+    <li><b>numerical_convergence_tests.ipynb:</b> A number of tests to investigate the numerical convergence obtained with the source number reduction.
+    <li><b>r_fesc.ipynb:</b> Used to analyze the properties including the escape fraction of individual stellar particles. Used to create fig. 10.
+    <li><b>histograms.ipynb:</b> Used to create 2D histograms to investigate the effect halo properties have on the escape fraction. This was used to create fig. 6. In addition, there are some tools developed to separate the two modes of escape fractions at lower stellar masses.
+    <li><b>spectra.ipynb:</b> Used to analyze the spectral dependence of the escape fraction as seen in figs. 11 and 12. 
+
+
+
+
 
 
     
--- prepare_new_runs.py
+++ prepare_new_runs.py
@@ -1,17 +1,19 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
 
-from crashpy.dataclasses import spectrum 
+from crashpy.dataclasses import spectrum
 import scipy.constants as constants
+
 
 def get_nearest_lum_source(faint_source, lum_sources):
     distances = []
     for _, source in lum_sources.iterrows():
-        distance = np.sqrt((source.x-faint_source.x)**2+
-                          (source.y-faint_source.y)**2+
-                          (source.z-faint_source.z)**2)
+        distance = np.sqrt(
+            (source.x - faint_source.x) ** 2
+            + (source.y - faint_source.y) ** 2
+            + (source.z - faint_source.z) ** 2
+        )
         distances.append(distance)
     distances = np.array(distances)
     min_index = np.argmin(distances)
@@ -19,164 +21,263 @@ def get_nearest_lum_source(faint_source, lum_sources):
     index = lum_sources.iloc[min_index].name
     return min_value, index
 
+
 def source_merger(source_df, lum_source_idx, faint_source_idx, path_to_halo_spec):
     faint_source = source_df.loc[faint_source_idx]
     lum_source = source_df.loc[lum_source_idx]
 
-    faint_spec_path = os.path.join(path_to_halo_spec, faint_source['name'][1:-1]+'.ps')
-    lum_spec_path = os.path.join(path_to_halo_spec, lum_source['name'][1:-1]+'.ps')
+    faint_spec_path = os.path.join(
+        path_to_halo_spec, faint_source["name"][1:-1] + ".ps"
+    )
+    lum_spec_path = os.path.join(path_to_halo_spec, lum_source["name"][1:-1] + ".ps")
 
     spec_faint = spectrum.CrashSpectrum.fromFile(faint_spec_path)
     spec_lum = spectrum.CrashSpectrum.fromFile(lum_spec_path)
 
-    lum_faint = faint_source['Q']
-    lum_lum = lum_source['Q']
+    lum_faint = faint_source["Q"]
+    lum_lum = lum_source["Q"]
 
     spec_faint.setEmissivity(lum_faint)
     spec_lum.setEmissivity(lum_lum)
 
-    summed_spectrum = spectrum.CrashSpectrum(constants.e/constants.h*spec_lum.spectrum['hnu'],
-                                             spec_lum.spectrum['Lnu']+spec_faint.spectrum['Lnu'])
+    summed_spectrum = spectrum.CrashSpectrum(
+        constants.e / constants.h * spec_lum.spectrum["hnu"],
+        spec_lum.spectrum["Lnu"] + spec_faint.spectrum["Lnu"],
+    )
 
     out_path = lum_spec_path
     summed_spectrum.toFile(path=out_path, f=True)
 
-    source_df.loc[lum_source_idx, 'Q'] = lum_faint+lum_lum
+    source_df.loc[lum_source_idx, "Q"] = lum_faint + lum_lum
     source_df.drop(faint_source_idx, inplace=True)
     return
 
-def merge_halo_sources(path_to_input, path_to_spectra, snap, halo_id, threshold=0.01, max_dist=1.9):
-    column_names=['x', 'y', 'z', 'a', 'Q', 'b', 'name', 'c',
-    'halo', 'd', 'packets', 'e', 'loc_uv', 'f', 'loc_x']
-    sources_path = os.path.join(path_to_input, snap, f'g{halo_id}','sources_ic00.in')
-    spectra_path = os.path.join(path_to_spectra, snap, f'g{halo_id}')
 
-    source_df = pd.read_table(sources_path, delimiter='\t', names=column_names)
-    source_df['Q'] = source_df['Q'].str.replace('d','e').astype('float')
+def merge_halo_sources(
+    path_to_input, path_to_spectra, snap, halo_id, threshold=0.01, max_dist=1.9
+):
+    column_names = [
+        "x",
+        "y",
+        "z",
+        "a",
+        "Q",
+        "b",
+        "name",
+        "c",
+        "halo",
+        "d",
+        "packets",
+        "e",
+        "loc_uv",
+        "f",
+        "loc_x",
+    ]
+    sources_path = os.path.join(path_to_input, snap, f"g{halo_id}", "sources_ic00.in")
+    spectra_path = os.path.join(path_to_spectra, snap, f"g{halo_id}")
 
-    lum_sources = source_df[source_df['Q']>threshold*source_df['Q'].max()]
-    faint_sources = source_df[source_df['Q']<threshold*source_df['Q'].max()]
+    source_df = pd.read_table(sources_path, delimiter="\t", names=column_names)
+    source_df["Q"] = source_df["Q"].str.replace("d", "e").astype("float")
+
+    lum_sources = source_df[source_df["Q"] > threshold * source_df["Q"].max()]
+    faint_sources = source_df[source_df["Q"] < threshold * source_df["Q"].max()]
     for faint_idx, source in faint_sources.iterrows():
-        dist, lum_source_idx = get_nearest_lum_source(faint_source=source, lum_sources=lum_sources)
+        dist, lum_source_idx = get_nearest_lum_source(
+            faint_source=source, lum_sources=lum_sources
+        )
         if dist > max_dist:
             source_df.drop(faint_idx, inplace=True)
         else:
-            source_merger(source_df=source_df, lum_source_idx=lum_source_idx,
-                          faint_source_idx=faint_idx,
-                          path_to_halo_spec=spectra_path)
+            source_merger(
+                source_df=source_df,
+                lum_source_idx=lum_source_idx,
+                faint_source_idx=faint_idx,
+                path_to_halo_spec=spectra_path,
+            )
 
-    source_df['Q'] = source_df['Q'].apply('{:.3e}'.format)
-    source_df['Q'] = source_df['Q'].str.replace('e','d')
-    source_df.to_csv(sources_path, sep='\t', header=False, index=False)
+    source_df["Q"] = source_df["Q"].apply("{:.3e}".format)
+    source_df["Q"] = source_df["Q"].str.replace("e", "d")
+    source_df.to_csv(sources_path, sep="\t", header=False, index=False)
     return
+
 
 def get_all_halos(path, snap):
     path_to_snap = os.path.join(path, snap)
     files = os.listdir(path_to_snap)
-    halos = [int(halo[1:]) for halo in files if halo.startswith('g')]
+    halos = [int(halo[1:]) for halo in files if halo.startswith("g")]
     return halos
 
+
 def all_halo_merge(path_to_input, path_to_spectra, threshold=0.01, max_dist=5):
-    snaps = ['sn004','sn008','sn013']
+    snaps = ["sn004", "sn008", "sn013"]
     for snap in snaps:
         halos = get_all_halos(path_to_input, snap)
         for halo in halos:
-            print(f'Merging sources in halo {halo} in snap {snap}')
-            column_names=['x', 'y', 'z', 'a', 'Q', 'b', 'name', 'c',
-            'halo', 'd', 'packets', 'e', 'loc_uv', 'f', 'loc_x']
-            sources_path = os.path.join(path_to_input, snap, f'g{halo}','sources_ic00.in')
-            spectra_path = os.path.join(path_to_spectra, snap, f'g{halo}')
+            print(f"Merging sources in halo {halo} in snap {snap}")
+            column_names = [
+                "x",
+                "y",
+                "z",
+                "a",
+                "Q",
+                "b",
+                "name",
+                "c",
+                "halo",
+                "d",
+                "packets",
+                "e",
+                "loc_uv",
+                "f",
+                "loc_x",
+            ]
+            sources_path = os.path.join(
+                path_to_input, snap, f"g{halo}", "sources_ic00.in"
+            )
+            spectra_path = os.path.join(path_to_spectra, snap, f"g{halo}")
             try:
-                source_df = pd.read_table(sources_path, delimiter='\t', names=column_names)
+                source_df = pd.read_table(
+                    sources_path, delimiter="\t", names=column_names
+                )
             except:
-                print(f'Halo {halo} in snap {snap} does not seem to have a sources file!')
-            source_df['Q'] = source_df['Q'].str.replace('d','e').astype('float')
+                print(
+                    f"Halo {halo} in snap {snap} does not seem to have a sources file!"
+                )
+            source_df["Q"] = source_df["Q"].str.replace("d", "e").astype("float")
 
-            lum_sources = source_df[source_df['Q']>threshold*source_df['Q'].max()]
-            faint_sources = source_df[source_df['Q']<threshold*source_df['Q'].max()]
+            lum_sources = source_df[source_df["Q"] > threshold * source_df["Q"].max()]
+            faint_sources = source_df[source_df["Q"] < threshold * source_df["Q"].max()]
             for faint_idx, source in faint_sources.iterrows():
-                dist, lum_source_idx = get_nearest_lum_source(faint_source=source, lum_sources=lum_sources)
+                dist, lum_source_idx = get_nearest_lum_source(
+                    faint_source=source, lum_sources=lum_sources
+                )
                 if dist > max_dist:
                     source_df.drop(faint_idx, inplace=True)
                 else:
-                    source_merger(source_df=source_df, lum_source_idx=lum_source_idx,
-                                  faint_source_idx=faint_idx,
-                                  path_to_halo_spec=spectra_path)
+                    source_merger(
+                        source_df=source_df,
+                        lum_source_idx=lum_source_idx,
+                        faint_source_idx=faint_idx,
+                        path_to_halo_spec=spectra_path,
+                    )
 
-            source_df['Q'] = source_df['Q'].apply('{:.3e}'.format)
-            source_df['Q'] = source_df['Q'].str.replace('e','d')
-            source_df.to_csv(sources_path, sep='\t', header=False, index=False)
+            source_df["Q"] = source_df["Q"].apply("{:.3e}".format)
+            source_df["Q"] = source_df["Q"].str.replace("e", "d")
+            source_df.to_csv(sources_path, sep="\t", header=False, index=False)
     return
 
+
 def set_packet_num(input_dir, packet_num):
-    column_names=['x', 'y', 'z', 'a', 'Q', 'b', 'name', 'c', 'halo', 'd', 'packets', 'e', 'loc_uv', 'f', 'loc_x']
+    column_names = [
+        "x",
+        "y",
+        "z",
+        "a",
+        "Q",
+        "b",
+        "name",
+        "c",
+        "halo",
+        "d",
+        "packets",
+        "e",
+        "loc_uv",
+        "f",
+        "loc_x",
+    ]
     for snap in os.listdir(input_dir):
         packets = packet_num
         snap_dir = os.path.join(input_dir, snap)
         for halo in os.listdir(snap_dir):
             try:
-                print(f'Setting the packet number to {packets} in halo {halo} of snap {snap}')
+                print(
+                    f"Setting the packet number to {packets} in halo {halo} of snap {snap}"
+                )
                 halo_dir = os.path.join(snap_dir, halo)
-                sources_path = os.path.join(halo_dir, 'sources_ic00.in')
+                sources_path = os.path.join(halo_dir, "sources_ic00.in")
 
-                sources = pd.read_table(sources_path, delimiter='\t', names=column_names)
+                sources = pd.read_table(
+                    sources_path, delimiter="\t", names=column_names
+                )
                 sources.packets = packets
-                sources.to_csv(sources_path, sep='\t', header=False, index=False)
+                sources.to_csv(sources_path, sep="\t", header=False, index=False)
             except:
                 print(halo)
                 continue
 
 
 def set_loc_esc(input_dir, esc_frac):
-    column_names=['x', 'y', 'z', 'a', 'Q', 'b', 'name', 'c', 'halo', 'd', 'packets', 'e', 'loc_uv', 'f', 'loc_x']
+    column_names = [
+        "x",
+        "y",
+        "z",
+        "a",
+        "Q",
+        "b",
+        "name",
+        "c",
+        "halo",
+        "d",
+        "packets",
+        "e",
+        "loc_uv",
+        "f",
+        "loc_x",
+    ]
     for snap in os.listdir(input_dir):
-        packets = packet_num
         snap_dir = os.path.join(input_dir, snap)
         for halo in os.listdir(snap_dir):
             try:
-                print(f'Setting the local escape fraction to {esc_frac} in halo {halo} of snap {snap}')
+                print(
+                    f"Setting the local escape fraction to {esc_frac} in halo {halo} of snap {snap}"
+                )
                 halo_dir = os.path.join(snap_dir, halo)
-                sources_path = os.path.join(halo_dir, 'sources_ic00.in')
+                sources_path = os.path.join(halo_dir, "sources_ic00.in")
 
-                sources = pd.read_table(sources_path, delimiter='\t', names=column_names)
+                sources = pd.read_table(
+                    sources_path, delimiter="\t", names=column_names
+                )
                 sources.loc_uv = esc_frac
                 sources.loc_x = esc_frac
-                sources.to_csv(sources_path, sep='\t', header=False, index=False)
+                sources.to_csv(sources_path, sep="\t", header=False, index=False)
             except:
                 print(halo)
                 continue
 
+
 def remove_heavy_halos(path, star_mass_lim=1e8):
-    snaps = ['sn004','sn008','sn013']
-    df_name = 'sel_halos_df.pickle'
+    snaps = ["sn004", "sn008", "sn013"]
+    df_name = "sel_halos_df.pickle"
     for snap in snaps:
         halos = get_all_halos(path, snap)
         df_path = os.path.join(path, snap, df_name)
         all_halos = pd.read_pickle(df_path)
         halos_used = all_halos.loc[halos]
-        
-        star_masses = halos_used[('GroupMassType', 4)]*1e10/0.6774
-        to_remove = np.array(star_masses[star_masses>star_mass_lim].index)
-        halos_to_remove = [f'g{halo}' for halo in to_remove]
-        
+
+        star_masses = halos_used[("GroupMassType", 4)] * 1e10 / 0.6774
+        to_remove = np.array(star_masses[star_masses > star_mass_lim].index)
+        halos_to_remove = [f"g{halo}" for halo in to_remove]
+
         for halo in halos_to_remove:
             path_to_halo = os.path.join(path, snap, halo)
-            print(f'Removing {path_to_halo}')
-            os.system(f'rm -r {path_to_halo}')
+            print(f"Removing {path_to_halo}")
+            os.system(f"rm -r {path_to_halo}")
     return
 
-def prepare_halos(conf, threshold=0.01, max_dist=5, packet_number='1.000d+08'):
-    basepath = '/ptmp/mpa/mglatzle/TNG_f_esc'
+
+def prepare_halos(conf, threshold=0.01, max_dist=5, packet_number="1.000d+08"):
+    basepath = "/ptmp/mpa/mglatzle/TNG_f_esc"
     path_to_conf = os.path.join(basepath, conf)
-    path_to_input = os.path.join(path_to_conf, 'input', 'L35n2160TNG')
-    path_to_spectra = os.path.join(path_to_conf, 'db', 'SPECTRA', 'L35n2160TNG')
+    path_to_input = os.path.join(path_to_conf, "input", "L35n2160TNG")
+    path_to_spectra = os.path.join(path_to_conf, "db", "SPECTRA", "L35n2160TNG")
 
     remove_heavy_halos(path_to_input, star_mass_lim=1e9)
     all_halo_merge(path_to_input, path_to_spectra, threshold=0.01, max_dist=5)
     set_packet_num(path_to_input, packet_number)
     return
 
-prepare_halos('new_1e-1')
-prepare_halos('new_5e-1')
-prepare_halos('new_7e-1')
-prepare_halos('new_dust')
+
+prepare_halos("new_1e-1")
+prepare_halos("new_5e-1")
+prepare_halos("new_7e-1")
+prepare_halos("new_dust")
